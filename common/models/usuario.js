4c0c0ecaffa70002d47ed2b1a5f419686c565fec
J.Pablo Estrada <pabloleeestrada@gmail.com>
2016-09-24 21:59:36 -0600

    fix error

--- common/models/usuario.js
+++ common/models/usuario.js
@@ -5,25 +5,47 @@ var app = require('../../server/server');
 module.exports = function(Usuario) {
   Usuario.login = function(email, password, nombreComercial, pais, cb) {
     var pyme = app.models.pyme;
+    var estado = app.models.estado;
+    var estados = [];
     var us = false;
     var pm = false;
+    var ps = false;
+    var es = false;
+    var usuarioid = 0;
+    // usuario
     // eslint-disable-next-line
     Usuario.find({where: {usuario: email, clave: password }}, function(err, user) {
       if (user.length > 0) {
         us = true;
+        usuarioid = user.id;
       } else {
         us = false;
       }
     });
+
+    // eslint-disable-next-line
+    estado.find({where: {paisid: pais}}, function(err, state) {
+      if (py.length > 0) {
+        estados = state;
+      } else {
+        pm = false;
+      }
+    });
+    // pyme
     // eslint-disable-next-line max-len
-    pyme.find({where: {nombrecomercio: email}}, function(err, py) {
+    pyme.find({where: {nombrecomercio: nombreComercial, usuarioid: usuarioid}}, function(err, py) {
+      estados.forEach(function(value, index) {
+        if (value == py.estadoid) ps = true;
+      });
       if (py.length > 0) {
         pm = true;
       } else {
         pm = false;
       }
     });
-    if (us && pm) {
+    // eslint-disable-next-line max-len
+
+    if (us && pm && ps) {
       cb(null, 'user logged');
     } else {
       cb(null, 'user password or username wrong');
